2e31f957ee8bf4b792fb9212f4515310cdc97f4c
ankita <pathakankita@seattleu.edu>
2019-04-19 23:45:48 -0700

    hw3 files

---
+++ productsdata.js
@@ -0,0 +1,139 @@
+{
+  "data": [
+    {
+      "ProductNumber": "101",
+      "ProductName": "Trek 9000 Mountain Bike",
+      "RetailPrice": "1200",
+      "QuantityOnHand": "6",
+      "ManufacturingDate": "2018/07/25",
+      "CategoryID": "2",
+      "CategoryDescription": "Bikes"
+    },
+    {
+      "ProductNumber": "102",
+      "ProductName": "Eagle FS-3 Mountain Bike",
+      "RetailPrice": "1800",
+      "QuantityOnHand": "8",
+      "ManufacturingDate": "2018/09/01",
+      "CategoryID": "2",
+      "CategoryDescription": "Bikes"
+    },
+    {
+      "ProductNumber": "103",
+      "ProductName": "Dog Ear Cyclecomputer",
+      "RetailPrice": "75",
+      "QuantityOnHand": "20",
+      "ManufacturingDate": "2017/04/22",
+      "CategoryID": "1",
+      "CategoryDescription": "Accessories"
+    },
+    {
+      "ProductNumber": "104",
+      "ProductName": "Victoria Pro All Weather Tires",
+      "RetailPrice": "54",
+      "QuantityOnHand": "20",
+      "ManufacturingDate": "2017/06/01",
+      "CategoryID": "4",
+      "CategoryDescription": "Components"
+    },
+    {
+      "ProductNumber": "105",
+      "ProductName": "Dog Ear Helmet Mount Mirrors",
+      "RetailPrice": "8",
+      "QuantityOnHand": "12",
+      "ManufacturingDate": "2014/05/22",
+      "CategoryID": "1",
+      "CategoryDescription": "Accessories"
+    },
+    {
+      "ProductNumber": "106",
+      "ProductName": "Viscount Mountain Bike",
+      "RetailPrice": "635",
+      "QuantityOnHand": "5",
+      "ManufacturingDate": "2016/03/31",
+      "CategoryID": "2",
+      "CategoryDescription": "Bikes"
+    },
+    {
+      "ProductNumber": "107",
+      "ProductName": "Viscount C-500 Wireless Bike Computer",
+      "RetailPrice": "49",
+      "QuantityOnHand": "30",
+      "ManufacturingDate": "2019/01/30",
+      "CategoryID": "1",
+      "CategoryDescription": "Accessories"
+    },
+    {
+      "ProductNumber": "108",
+      "ProductName": "Kryptonite Advanced 2000 U-Lock",
+      "RetailPrice": "50",
+      "QuantityOnHand": "20",
+      "ManufacturingDate": "2017/09/01",
+      "CategoryID": "1",
+      "CategoryDescription": "Accessories"
+    },
+    {
+      "ProductNumber": "109",
+      "ProductName": "Nikoma Lok-Tight U-Lock",
+      "RetailPrice": "33",
+      "QuantityOnHand": "12",
+      "ManufacturingDate": "2017/06/01",
+      "CategoryID": "1",
+      "CategoryDescription": "Accessories"
+    },
+    {
+      "ProductNumber": "110",
+      "ProductName": "Ultra-Pro Rain Jacket",
+      "RetailPrice": "85",
+      "QuantityOnHand": "30",
+      "ManufacturingDate": "2018/01/03",
+      "CategoryID": "3",
+      "CategoryDescription": "Clothing"
+    },
+    {
+      "ProductNumber": "111",
+      "ProductName": "StaDry Cycling Pants",
+      "RetailPrice": "69",
+      "QuantityOnHand": "8",
+      "ManufacturingDate": "2016/05/20",
+      "CategoryID": "3",
+      "CategoryDescription": "Clothing"
+    },
+    {
+      "ProductNumber": "112",
+      "ProductName": "X-Pro All Weather Tires",
+      "RetailPrice": "24",
+      "QuantityOnHand": "20",
+      "ManufacturingDate": "2017/07/08",
+      "CategoryID": "6",
+      "CategoryDescription": "Tires"
+    },
+    {
+      "ProductNumber": "113",
+      "ProductName": "Turbo Twin Tires",
+      "RetailPrice": "29",
+      "QuantityOnHand": "16",
+      "ManufacturingDate": "2017/08/31",
+      "CategoryID": "6",
+      "CategoryDescription": "Tires"
+    },
+    {
+      "ProductNumber": "114",
+      "ProductName": "Cosmic Elite Road Warrior Wheels",
+      "RetailPrice": "165",
+      "QuantityOnHand": "22",
+      "ManufacturingDate": "2016/05/07",
+      "CategoryID": "4",
+      "CategoryDescription": "Components"
+    },
+    {
+      "ProductNumber": "115",
+      "ProductName": "Road Warrior Hitch Pack",
+      "RetailPrice": "175",
+      "QuantityOnHand": "8",
+      "ManufacturingDate": "2017/09/26",
+      "CategoryID": "5",
+      "CategoryDescription": "Car Racks"
+    }
+  ]
+}
